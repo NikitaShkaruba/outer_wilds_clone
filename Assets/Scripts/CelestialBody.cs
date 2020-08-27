0a1999d3001067d06bfb3ef6036ddfc975f7c957
unknown <sh.sigmaone@gmail.com>
2020-08-27 13:54:44 +0300

    Add velocity computation for celestial bodies

---
+++ Assets/Scripts/CelestialBody.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CelestialBody : MonoBehaviour
+{
+    [HideInInspector] public new Rigidbody rigidbody;
+
+    // Movement
+    public float mass;
+    [SerializeField] private Vector3 initialVelocity;
+    private Vector3 currentVelocity;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+
+        currentVelocity = initialVelocity;
+    }
+
+    public void AddVelocity(Vector3 velocity)
+    {
+        currentVelocity += velocity;
+    }
+
+    public void UpdatePosition()
+    {
+        rigidbody.position += currentVelocity;
+    }
+}
---
+++ Assets/Scripts/SolarSystem.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class SolarSystem : MonoBehaviour
+{
+    private const float GravitationalConstant = 1f;
+
+    private CelestialBody[] celestialBodies;
+
+    private void Awake()
+    {
+        celestialBodies = FindObjectsOfType<CelestialBody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float deltaTime = Time.deltaTime * 100;
+
+        foreach (CelestialBody celestialBody in celestialBodies)
+        {
+            Vector3 velocity = ComputeCelestialBodyVelocity(celestialBody);
+            
+            celestialBody.AddVelocity(velocity * deltaTime);
+            celestialBody.UpdatePosition();
+        }
+    }
+
+    private Vector3 ComputeCelestialBodyVelocity(CelestialBody celestialBody)
+    {
+        Vector3 velocity = Vector3.zero;
+
+        foreach (CelestialBody otherCelestialBody in celestialBodies)
+        {
+            // Don't add force to itself
+            if (celestialBody == otherCelestialBody)
+            {
+                continue;
+            }
+
+            // Newton's law of universal gravitation F = G * (m1 * m2 / r^2)
+            Vector3 differenceWithOtherBody = otherCelestialBody.rigidbody.position - celestialBody.rigidbody.position;
+            float destinationSquare = differenceWithOtherBody.sqrMagnitude;
+            Vector3 forceDirection = differenceWithOtherBody.normalized;
+            Vector3 force = forceDirection * GravitationalConstant * (celestialBody.mass * otherCelestialBody.mass / destinationSquare);
+            Vector3 acceleration = force / celestialBody.mass;
+
+            velocity += acceleration;
+        }
+
+        return velocity;
+    }
+}
