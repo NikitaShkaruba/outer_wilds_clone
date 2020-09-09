b859ac9e9f049074dbbf387a7fd8ceb88ccc346f
Nikita Shkaruba <sh.sigmaone@gmail.com>
2020-09-10 00:33:42 +0300

    Add gravity field pull up

---
+++ Assets/Scripts/Tools/GravityField.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace Tools
+{
+    public class GravityField : MonoBehaviour
+    {
+        private Player pulledPlayer;
+
+        private void FixedUpdate()
+        {
+            if (pulledPlayer == null)
+            {
+                return;
+            }
+
+            Transform cachedTransform = transform;
+
+            Vector3 force = cachedTransform.position - pulledPlayer.Position; // Pull towards the center
+            force += cachedTransform.up * 8f; // Pull up
+            force *= 1000f; // Add more power
+            force *= Time.deltaTime; // Scale with physics step
+
+            pulledPlayer.rigidbody.AddForce(force);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Player player = other.GetComponent<Player>();
+            if (player == null)
+            {
+                return;
+            }
+
+            pulledPlayer = player;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            Player collidedPlayer = other.GetComponent<Player>();
+            if (collidedPlayer == null)
+            {
+                return;
+            }
+
+            pulledPlayer = null;
+        }
+    }
+}
