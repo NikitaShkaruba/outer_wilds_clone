517dd38c3c557be78bd03f407b796e93d07ea067
Nikita Shkaruba <sh.sigmaone@gmail.com>
2020-09-24 22:51:06 +0300

    Rework buggy SpaceShipGravityField player pull

--- Assets/Scripts/Tools/SpaceShipParts/SpaceShipGravityField.cs
+++ Assets/Scripts/Tools/SpaceShipParts/SpaceShipGravityField.cs
@@ -4,13 +4,12 @@ namespace Tools.SpaceShipParts
 {
     public class SpaceShipGravityField : MonoBehaviour
     {
-        private Player pulledPlayer;
-
         public float pullPower;
 
-        private void FixedUpdate()
+        private void OnTriggerStay(Collider other)
         {
-            if (pulledPlayer == null)
+            Player collidedPlayer = other.GetComponent<Player>();
+            if (collidedPlayer == null)
             {
                 return;
             }
@@ -19,31 +18,9 @@ namespace Tools.SpaceShipParts
 
             Vector3 force = cachedTransform.up; // Pull up
             force *= pullPower; // Add power
-            force *= Time.deltaTime; // Scale with physics step
+            force *= Time.fixedDeltaTime; // Scale with physics step
 
-            pulledPlayer.rigidbody.AddForce(force);
-        }
-
-        private void OnTriggerEnter(Collider other)
-        {
-            Player collidedPlayer = other.GetComponent<Player>();
-            if (collidedPlayer == null)
-            {
-                return;
-            }
-
-            pulledPlayer = collidedPlayer;
-        }
-
-        private void OnTriggerExit(Collider other)
-        {
-            Player collidedPlayer = other.GetComponent<Player>();
-            if (collidedPlayer == null)
-            {
-                return;
-            }
-
-            pulledPlayer = null;
+            collidedPlayer.rigidbody.AddForce(force);
         }
     }
 }
