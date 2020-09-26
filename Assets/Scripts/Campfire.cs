d70f74f086c42006c0fe6d7f4ec84a152608883e
Nikita Shkaruba <sh.sigmaone@gmail.com>
2020-09-27 01:04:31 +0300

    Add bonfire which hurts the player

---
+++ Assets/Scripts/Campfire.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Campfire : MonoBehaviour
+{
+    [SerializeField] private float fireDamage;
+
+    private void OnTriggerStay(Collider other)
+    {
+        Player collidedPlayer = other.GetComponent<Player>();
+        if (collidedPlayer == null)
+        {
+            return;
+        }
+
+        collidedPlayer.Hurt(fireDamage);
+    }
+}
