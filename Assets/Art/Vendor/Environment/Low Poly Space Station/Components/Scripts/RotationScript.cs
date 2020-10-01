027b07cdd6c4c6683338eb3294d9434b3ef17cab
Nikita Shkaruba <sh.sigmaone@gmail.com>
2020-10-01 22:08:55 +0300

    Move art from GameObjects to Art directory

---
+++ Assets/Art/Vendor/Environment/Low Poly Space Station/Components/Scripts/RotationScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class RotationScript : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        // Rotate the object around its local Y axis at 1 degree per second
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+        transform.Rotate(Vector3.left * speed * Time.deltaTime);
+    }
+}
